5b17c44366349c91276c06384492b099740dff4c
laa <lomakin.andrey@gmail.com>
2018-02-13 13:38:21 +0200

    Deletion of rings was added to the crash test

--- src/main/java/com/orientechnologies/crashtest/Loader.java
+++ src/main/java/com/orientechnologies/crashtest/Loader.java
@@ -3,6 +3,7 @@ package com.orientechnologies.crashtest;
 import com.orientechnologies.common.concur.ONeedRetryException;
 import com.orientechnologies.orient.core.db.ODatabasePool;
 import com.orientechnologies.orient.core.db.ODatabaseSession;
+import com.orientechnologies.orient.core.record.ODirection;
 import com.orientechnologies.orient.core.record.OEdge;
 import com.orientechnologies.orient.core.record.OVertex;
 import com.orientechnologies.orient.core.sql.executor.OResult;
@@ -47,136 +48,36 @@ class Loader implements Callable<Void> {
   public Void call() {
     try {
       final ThreadLocalRandom random = ThreadLocalRandom.current();
-      int ringsCounter = 0;
+      int ringsCreationCounter = 0;
+      int ringsDeletionCounter = 0;
 
       while (!stopFlag.get()) {
-        try (ODatabaseSession session = pool.acquire()) {
-          final int ringSize = random.nextInt(10) + 3;
+        try {
+          if (random.nextDouble() > 0.1) {
+            final int ringsCount = addRing(random);
 
-          try {
-            session.begin();
-
-            final long ringId = idGen.getAndIncrement();
-
-            OVertex prevVertex = null;
-            OVertex firstVertex = null;
-
-            final Set<Integer> vertexIds = new HashSet<>();
-
-            while (vertexIds.size() < ringSize) {
-              final int vertexId = random.nextInt(DataLoader.VERTEX_COUNT);
-              vertexIds.add(vertexId);
+            if (ringsCount == -1) {
+              return null;
             }
 
-            final List<OVertex> vertices = fetchVertices(session, vertexIds);
+            ringsCreationCounter += ringsCount;
 
-            for (int i = 0; i < vertices.size(); i++) {
-              OVertex vertex = vertices.get(i);
-              List<Long> ringIds = vertex.getProperty(DataLoader.RING_IDS);
-
-              int retryCounter = 0;
-
-              while (ringIds != null && ringIds.size() >= 60) {
-                retryCounter++;
-
-                if (retryCounter > MAX_RETRIES) {
-                  logger.info("%s - Limit of ring retries is reached, thread stopped, %d rings were created",
-                      Thread.currentThread().getName(), ringsCounter);
-                  return null;
-                }
-
-                vertex = chooseRandomVertex(session, random);
-                while (!vertexIds.add(vertex.<Integer>getProperty(DataLoader.V_ID))) {
-                  retryCounter++;
-
-                  if (retryCounter > MAX_RETRIES) {
-                    logger.info("%s - Limit of ring retries is reached, thread stopped, %d rings were created",
-                        Thread.currentThread().getName(), ringsCounter);
-                    return null;
-                  }
-
-                  vertex = chooseRandomVertex(session, random);
-                }
-
-                ringIds = vertex.getProperty(DataLoader.RING_IDS);
-                vertices.set(i, vertex);
-              }
-
-              if (ringIds == null) {
-                ringIds = new ArrayList<>();
-                vertex.setProperty(DataLoader.RING_IDS, ringIds);
-              }
-
-              ringIds.add(ringId);
-
-              List<Integer> ringSizes = vertex.getProperty(DataLoader.RING_SIZES);
-              if (ringSizes == null) {
-                ringSizes = new ArrayList<>();
-                vertex.setProperty(DataLoader.RING_SIZES, ringSizes);
-              }
-
-              ringSizes.add(ringSize);
-
-              if (prevVertex != null) {
-                final OEdge edge = prevVertex.addEdge(vertex, DataLoader.CRASH_E);
-                edge.setProperty(DataLoader.RING_ID, ringId);
-
-                if (addIndex) {
-                  addRandomValues(random, edge);
-                }
-
-                if (addBinaryRecrods) {
-                  addBinaryRecord(random, edge);
-                }
-
-                prevVertex.save();
-                edge.save();
-              }
-
-              vertex.save();
-              prevVertex = vertex;
-
-              if (firstVertex == null) {
-                firstVertex = vertex;
-              }
+            if (ringsCreationCounter > 0 && ringsCreationCounter % 1000 == 0) {
+              logger.info("%s thread, %d rings were created", Thread.currentThread().getName(), ringsCreationCounter);
             }
+          } else {
+            ringsDeletionCounter += removeRing(random);
 
-            if (prevVertex != null) {
-              OEdge edge = prevVertex.addEdge(firstVertex, DataLoader.CRASH_E);
-              edge.setProperty(DataLoader.RING_ID, ringId);
-
-              if (addIndex) {
-                addRandomValues(random, edge);
-              }
-
-              if (addBinaryRecrods) {
-                addBinaryRecord(random, edge);
-              }
-
-              edge.save();
-              prevVertex.save();
-              firstVertex.save();
+            if (ringsDeletionCounter > 0 && ringsDeletionCounter % 10 == 0) {
+              logger.info("%s thread, %d rings were deleted", Thread.currentThread().getName(), ringsDeletionCounter);
             }
-
-            if (DataLoader.generateOOM.get()) {
-              addChunkToHeap(random);
-            }
-
-            session.commit();
-            ringsCounter++;
-
-            if (ringsCounter > 0 && ringsCounter % 1000 == 0) {
-              logger.info("%s thread, %d rings were created", Thread.currentThread().getName(), ringsCounter);
-            }
-          } catch (ONeedRetryException e) {
-            //continue;
-          } catch (OutOfMemoryError e) {
-            payLoad = null;
-            System.gc();
-            payLoad = new ArrayList<>();
-
-            logger.error("OOM in loader thread, ignore and repeat");
           }
+        } catch (OutOfMemoryError e) {
+          payLoad = null;
+          System.gc();
+          payLoad = new ArrayList<>();
+
+          logger.error("OOM in loader thread, ignore and repeat");
         }
       }
     } catch (RuntimeException | Error e) {
@@ -186,7 +87,211 @@ class Loader implements Callable<Void> {
     if (stopFlag.get()) {
       logger.info("Thread %s was stopped, by stop file", Thread.currentThread().getName());
     }
+
     return null;
+  }
+
+  private int removeRing(ThreadLocalRandom random) {
+    try (ODatabaseSession session = pool.acquire()) {
+      final int vertexId = random.nextInt(DataLoader.VERTEX_COUNT);
+      final OVertex ringsStart;
+
+      try (OResultSet resultSet = session
+          .query("select from " + DataLoader.CRASH_V + " where " + DataLoader.V_ID + " = ?", vertexId)) {
+        final OResult result = resultSet.next();
+
+        assert result.getVertex().isPresent();
+        ringsStart = result.getVertex().get();
+      }
+
+      List<Long> ringIds = ringsStart.getProperty(DataLoader.RING_IDS);
+      if (ringIds == null || ringIds.isEmpty()) {
+        return 0;
+      }
+
+      session.begin();
+      int ringIndex = random.nextInt(ringIds.size());
+
+      final long ringId = ringIds.get(ringIndex);
+      final List<OEdge> edgesToDelete = new ArrayList<>();
+
+      ringIds.remove(ringIndex);
+      List<Integer> ringSizes = ringsStart.getProperty(DataLoader.RING_SIZES);
+      ringSizes.remove(ringIndex);
+
+      ringsStart.save();
+
+      OVertex nextVertex = ringsStart;
+      do {
+        OVertex prevVertex = nextVertex;
+
+        for (OEdge edge : nextVertex.getEdges(ODirection.OUT, DataLoader.CRASH_E)) {
+          if (edge.getProperty(DataLoader.RING_ID).equals(ringId)) {
+            edgesToDelete.add(edge);
+            nextVertex = edge.getTo();
+            break;
+          }
+        }
+
+        if (prevVertex.getIdentity().equals(nextVertex.getIdentity())) {
+          throw new IllegalStateException("From and to vertices of rings can not be identical");
+        }
+
+        if (!nextVertex.getIdentity().equals(ringsStart.getIdentity())) {
+          ringIds = nextVertex.getProperty(DataLoader.RING_IDS);
+
+          ringIndex = ringIds.indexOf(ringId);
+          if (ringIndex < 0) {
+            throw new IllegalStateException("Ring id is absent in vertex");
+          }
+
+          ringIds.remove(ringIndex);
+
+          ringSizes = nextVertex.getProperty(DataLoader.RING_SIZES);
+          ringSizes.remove(ringIndex);
+
+          nextVertex.save();
+        }
+
+      } while (!nextVertex.getIdentity().equals(ringsStart.getIdentity()));
+
+      for (OEdge edge : edgesToDelete) {
+        edge.delete();
+      }
+
+      session.commit();
+
+    } catch (ONeedRetryException e) {
+      return 0;
+    }
+
+    return 1;
+  }
+
+  private int addRing(ThreadLocalRandom random) {
+    int ringsCounter = 0;
+
+    try (ODatabaseSession session = pool.acquire()) {
+      final int ringSize = random.nextInt(10) + 3;
+
+      try {
+        session.begin();
+
+        final long ringId = idGen.getAndIncrement();
+
+        OVertex prevVertex = null;
+        OVertex firstVertex = null;
+
+        final Set<Integer> vertexIds = new HashSet<>();
+
+        while (vertexIds.size() < ringSize) {
+          final int vertexId = random.nextInt(DataLoader.VERTEX_COUNT);
+          vertexIds.add(vertexId);
+        }
+
+        final List<OVertex> vertices = fetchVertices(session, vertexIds);
+
+        for (int i = 0; i < vertices.size(); i++) {
+          OVertex vertex = vertices.get(i);
+          List<Long> ringIds = vertex.getProperty(DataLoader.RING_IDS);
+
+          int retryCounter = 0;
+
+          while (ringIds != null && ringIds.size() >= 60) {
+            retryCounter++;
+
+            if (retryCounter > MAX_RETRIES) {
+              logger.info("%s - Limit of ring retries is reached, thread stopped, %d rings were created",
+                  Thread.currentThread().getName(), ringsCounter);
+              return -1;
+            }
+
+            vertex = chooseRandomVertex(session, random);
+            while (!vertexIds.add(vertex.<Integer>getProperty(DataLoader.V_ID))) {
+              retryCounter++;
+
+              if (retryCounter > MAX_RETRIES) {
+                logger.info("%s - Limit of ring retries is reached, thread stopped, %d rings were created",
+                    Thread.currentThread().getName(), ringsCounter);
+                return -1;
+              }
+
+              vertex = chooseRandomVertex(session, random);
+            }
+
+            ringIds = vertex.getProperty(DataLoader.RING_IDS);
+            vertices.set(i, vertex);
+          }
+
+          if (ringIds == null) {
+            ringIds = new ArrayList<>();
+            vertex.setProperty(DataLoader.RING_IDS, ringIds);
+          }
+
+          ringIds.add(ringId);
+
+          List<Integer> ringSizes = vertex.getProperty(DataLoader.RING_SIZES);
+          if (ringSizes == null) {
+            ringSizes = new ArrayList<>();
+            vertex.setProperty(DataLoader.RING_SIZES, ringSizes);
+          }
+
+          ringSizes.add(ringSize);
+
+          if (prevVertex != null) {
+            final OEdge edge = prevVertex.addEdge(vertex, DataLoader.CRASH_E);
+            edge.setProperty(DataLoader.RING_ID, ringId);
+
+            if (addIndex) {
+              addRandomValues(random, edge);
+            }
+
+            if (addBinaryRecrods) {
+              addBinaryRecord(random, edge);
+            }
+
+            prevVertex.save();
+            edge.save();
+          }
+
+          vertex.save();
+          prevVertex = vertex;
+
+          if (firstVertex == null) {
+            firstVertex = vertex;
+          }
+        }
+
+        if (prevVertex != null) {
+          OEdge edge = prevVertex.addEdge(firstVertex, DataLoader.CRASH_E);
+          edge.setProperty(DataLoader.RING_ID, ringId);
+
+          if (addIndex) {
+            addRandomValues(random, edge);
+          }
+
+          if (addBinaryRecrods) {
+            addBinaryRecord(random, edge);
+          }
+
+          edge.save();
+          prevVertex.save();
+          firstVertex.save();
+        }
+
+        if (DataLoader.generateOOM.get()) {
+          addChunkToHeap(random);
+        }
+
+        session.commit();
+        ringsCounter++;
+
+      } catch (ONeedRetryException e) {
+        //continue;
+      }
+    }
+
+    return ringsCounter;
   }
 
   private void addRandomValues(ThreadLocalRandom random, OEdge edge) {
@@ -237,7 +342,7 @@ class Loader implements Callable<Void> {
   }
 
   private void addChunkToHeap(ThreadLocalRandom random) {
-    final int chunkSize = random.nextInt(5 * 1024*1024) + 5 * 1024*1024;
+    final int chunkSize = random.nextInt(5 * 1024 * 1024) + 5 * 1024 * 1024;
     final byte[] chunk = new byte[chunkSize];
     payLoad.add(chunk);
   }
